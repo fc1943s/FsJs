f0c8e5b85c70fca32f73ba35408a59279ec6045a
fc1943s <root@fc1943s.dev>
2021-08-18 17:25:33 -0300

    ci

--- src/FsJs/pack.ps1
+++ src/FsJs/pack.ps1
@@ -1,4 +1,4 @@
 dotnet tool restore
 dotnet paket restore
 dotnet build --configuration Release
-dotnet paket pack bin/Release --build-config Release --symbols --include-referenced-projects
+dotnet paket pack bin/Release --build-config Release
