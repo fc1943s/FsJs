44c80f04cd1bde9d9cc70e31e8ceb3c6fdb76555
fc1943s <root@fc1943s.dev>
2021-08-18 15:17:54 -0300

    ci

--- src/FsJs/src/Dom.fs
+++ src/FsJs/src/Dom.fs
@@ -6,8 +6,7 @@ open Fable.Extras
 open FsCore
 open System.Collections.Generic
 open Fable.Core.JsInterop
-open Fable.Core
-open FsCore.Model
+open FsCore.BaseModel
 
 
 module Dom =
@@ -87,7 +86,7 @@ module Dom =
 
             let deviceId =
                 match window.localStorage.getItem "deviceId" with
-                | String.ValidString deviceId -> DeviceId (Guid deviceId)
+                | String.Valid deviceId -> DeviceId (Guid deviceId)
                 | _ ->
                     let deviceId = DeviceId.NewId ()
                     window.localStorage.setItem ("deviceId", deviceId |> DeviceId.Value |> string)
@@ -130,177 +129,12 @@ module Dom =
         let debug = Global.get "Debug" false
         debug <> false && (debug || isDebugStatic)
 
-
-
-    module ConsoleFlag =
-        let reset = "\x1b[0m"
-        let bright = "\x1b[1m"
-        let dim = "\x1b[2m"
-        let underscore = "\x1b[4m"
-        let blink = "\x1b[5m"
-        let reverse = "\x1b[7m"
-        let hidden = "\x1b[8m"
-
-        let fgBlack = "\x1b[30m"
-        let fgRed = "\x1b[31m"
-        let fgGreen = "\x1b[32m"
-        let fgYellow = "\x1b[33m"
-        let fgBlue = "\x1b[34m"
-        let fgMagenta = "\x1b[35m"
-        let fgCyan = "\x1b[36m"
-        let fgWhite = "\x1b[37m"
-
-        let fg =
-            [
-                fgBlack
-                fgRed
-                fgGreen
-                fgYellow
-                fgBlue
-                fgMagenta
-                fgCyan
-                fgWhite
-            ]
-
-        let bgBlack = "\x1b[40m"
-        let bgRed = "\x1b[41m"
-        let bgGreen = "\x1b[42m"
-        let bgYellow = "\x1b[43m"
-        let bgBlue = "\x1b[44m"
-        let bgMagenta = "\x1b[45m"
-        let bgCyan = "\x1b[46m"
-        let bgWhite = "\x1b[47m"
-
-
     let deviceTag =
         deviceInfo.DeviceId
         |> DeviceId.Value
         |> string
         |> String.substringFrom -4
 
-    let inline logWithFn logFn fn =
-        let result = fn ()
-
-        if result |> Option.ofObjUnbox |> Option.isSome then
-
-            let output =
-                [|
-                    let tagValue =
-                        deviceTag
-                        |> Seq.map Char.getNumericValue
-                        |> Seq.map Math.Abs
-                        |> Seq.map float
-                        |> Seq.sum
-
-                    let tagIndex = ((tagValue / 60.) * 5.) - 5. |> int
-
-                    //                    printfn $"tagValue={tagValue} tagIndex={tagIndex}"
-
-                    ConsoleFlag.fg.[Math.Min (ConsoleFlag.fg.Length - 1, Math.Max (0, tagIndex))]
-                    $"""[{deviceTag} {DateTime.Now |> DateTime.format "HH:mm:ss"}]"""
-                    ConsoleFlag.reset
-                    yield! result
-                |]
-                |> String.concat " "
-
-            logFn output
-
-    let inline log fn = logWithFn (fun x -> printfn $"{x}") fn
-
-    let inline logArray (fn: unit -> _ []) = logWithFn (fun x -> printfn $"{x}") fn
-    let inline elog fn = logWithFn (fun x -> eprintfn $"{x}") fn
-
-    let inline logFiltered newValue fn =
-        log
-            (fun () ->
-                if (string newValue).StartsWith "Ping " then
-                    null
-                else
-                    let result: string = fn ()
-
-                    if result.Contains "devicePing" then
-                        null
-                    else
-                        [|
-                            result
-                        |])
-
-    let inline consoleLog (x: _ []) = emitJsExpr x "console.log(...$0)"
-    let inline consoleError x = Browser.Dom.console.error x
-
-    type LogLevel =
-        | Trace = 0
-        | Debug = 1
-        | Info = 2
-        | Warning = 3
-        | Error = 4
-        | Critical = 5
-
-    let DEFAULT_LOG_LEVEL = if isDebug () then LogLevel.Debug else LogLevel.Info
-
-    type LogFn = (unit -> string) -> unit
-
-    type Logger =
-        {
-            Trace: LogFn
-            Debug: LogFn
-            Info: LogFn
-            Warning: LogFn
-            Error: LogFn
-        }
-
-    let logIf currentLogLevel logLevel (fn: unit -> string) =
-        if currentLogLevel <= logLevel then
-            let result = fn ()
-
-            if result |> Option.ofObjUnbox |> Option.isSome then
-                logArray
-                    (fun () ->
-                        [|
-                            match logLevel with
-                            | LogLevel.Trace -> ConsoleFlag.fgBlack
-                            | LogLevel.Debug -> ConsoleFlag.fgGreen
-                            | LogLevel.Info -> ConsoleFlag.fgWhite
-                            | LogLevel.Warning -> ConsoleFlag.fgYellow
-                            | LogLevel.Error -> ConsoleFlag.fgRed
-                            | LogLevel.Critical -> ConsoleFlag.fgMagenta
-                            | _ -> ConsoleFlag.fgWhite
-                            $"[{Enum.name logLevel}]"
-                            ConsoleFlag.fgWhite
-                            result
-                        |])
-
-    type Logger with
-
-
-
-
-
-        static member inline Create currentLogLevel =
-            let log = logIf currentLogLevel
-
-            {
-                Trace = log LogLevel.Trace
-                Debug = log LogLevel.Debug
-                Info = log LogLevel.Info
-                Warning = log LogLevel.Warning
-                Error = log LogLevel.Error
-            }
-
-        static member inline Default = Logger.Create DEFAULT_LOG_LEVEL
-
-    module Logger =
-        let mutable lastLogger = Logger.Default
-
-        let inline getLogger () = lastLogger // |> Option.defaultValue Logger.Default
-
-    let inline logTrace fn = Logger.getLogger().Trace fn
-    let inline logDebug fn = Logger.getLogger().Debug fn
-    let inline logInfo fn = Logger.getLogger().Info fn
-    let inline logWarning fn = Logger.getLogger().Warning fn
-    let inline logError fn = Logger.getLogger().Error fn
-
-    logInfo (fun () -> $"Dom. deviceInfo={JS.JSON.stringify deviceInfo}")
 
 
     let inline exited () =
@@ -354,7 +188,7 @@ module Dom =
                     if deviceInfo.IsTesting then
                         do! Js.sleep 0
                     else
-                        logInfo (fun () -> $"waitForSome: none. waiting... {fn.ToString ()}")
+                        printfn $"waitForSome: none. waiting... {fn.ToString ()}"
 
                         do! Js.sleep 100
 
---
+++ src/FsJs/src/Logger.fs
@@ -0,0 +1,168 @@
+namespace FsJs
+
+open System
+open FsCore
+open Fable.Core.JsInterop
+open Fable.Core
+
+
+module Logger =
+    module ConsoleFlag =
+        let reset = "\x1b[0m"
+        let bright = "\x1b[1m"
+        let dim = "\x1b[2m"
+        let underscore = "\x1b[4m"
+        let blink = "\x1b[5m"
+        let reverse = "\x1b[7m"
+        let hidden = "\x1b[8m"
+
+        let fgBlack = "\x1b[30m"
+        let fgRed = "\x1b[31m"
+        let fgGreen = "\x1b[32m"
+        let fgYellow = "\x1b[33m"
+        let fgBlue = "\x1b[34m"
+        let fgMagenta = "\x1b[35m"
+        let fgCyan = "\x1b[36m"
+        let fgWhite = "\x1b[37m"
+
+        let bgBlack = "\x1b[40m"
+        let bgRed = "\x1b[41m"
+        let bgGreen = "\x1b[42m"
+        let bgYellow = "\x1b[43m"
+        let bgBlue = "\x1b[44m"
+        let bgMagenta = "\x1b[45m"
+        let bgCyan = "\x1b[46m"
+        let bgWhite = "\x1b[47m"
+
+        let fg =
+            [
+                fgBlack
+                fgRed
+                fgGreen
+                fgYellow
+                fgBlue
+                fgMagenta
+                fgCyan
+                fgWhite
+            ]
+
+    let inline consoleLog (x: _ []) = emitJsExpr x "console.log(...$0)"
+    let inline consoleError x = Browser.Dom.console.error x
+
+    let inline logWithFn logFn fn =
+        let result = fn ()
+
+        if result |> Option.ofObjUnbox |> Option.isSome then
+
+            let output =
+                [|
+                    let tagValue =
+                        Dom.deviceTag
+                        |> Seq.map Char.getNumericValue
+                        |> Seq.map Math.Abs
+                        |> Seq.map float
+                        |> Seq.sum
+
+                    let tagIndex = ((tagValue / 60.) * 5.) - 5. |> int
+
+                    //                    printfn $"tagValue={tagValue} tagIndex={tagIndex}"
+
+                    ConsoleFlag.fg.[Math.Min (ConsoleFlag.fg.Length - 1, Math.Max (0, tagIndex))]
+                    $"""[{Dom.deviceTag} {DateTime.Now |> DateTime.format "HH:mm:ss"}]"""
+                    ConsoleFlag.reset
+                    yield! result
+                |]
+                |> String.concat " "
+
+            logFn output
+
+    let inline log fn = logWithFn (fun x -> printfn $"{x}") fn
+
+    let inline logFiltered newValue fn =
+        log
+            (fun () ->
+                if (string newValue).StartsWith "Ping " then
+                    null
+                else
+                    let result: string = fn ()
+
+                    if result.Contains "devicePing" then
+                        null
+                    else
+                        [|
+                            result
+                        |])
+
+    let inline logArray (fn: unit -> _ []) = logWithFn (fun x -> printfn $"{x}") fn
+    let inline elog fn = logWithFn (fun x -> eprintfn $"{x}") fn
+
+
+    type LogLevel =
+        | Trace = 0
+        | Debug = 1
+        | Info = 2
+        | Warning = 3
+        | Error = 4
+        | Critical = 5
+
+    let DEFAULT_LOG_LEVEL = if Dom.isDebug () then LogLevel.Debug else LogLevel.Info
+
+    type LogFn = (unit -> string) -> unit
+
+    type Logger =
+        {
+            Trace: LogFn
+            Debug: LogFn
+            Info: LogFn
+            Warning: LogFn
+            Error: LogFn
+        }
+
+    let logIf currentLogLevel logLevel (fn: unit -> string) =
+        if currentLogLevel <= logLevel then
+            let result = fn ()
+
+            if result |> Option.ofObjUnbox |> Option.isSome then
+                logArray
+                    (fun () ->
+                        [|
+                            match logLevel with
+                            | LogLevel.Trace -> ConsoleFlag.fgBlack
+                            | LogLevel.Debug -> ConsoleFlag.fgGreen
+                            | LogLevel.Info -> ConsoleFlag.fgWhite
+                            | LogLevel.Warning -> ConsoleFlag.fgYellow
+                            | LogLevel.Error -> ConsoleFlag.fgRed
+                            | LogLevel.Critical -> ConsoleFlag.fgMagenta
+                            | _ -> ConsoleFlag.fgWhite
+                            $"[{Enum.name logLevel}]"
+                            ConsoleFlag.fgWhite
+                            result
+                        |])
+
+    type Logger with
+
+        static member inline Create currentLogLevel =
+            let log = logIf currentLogLevel
+
+            {
+                Trace = log LogLevel.Trace
+                Debug = log LogLevel.Debug
+                Info = log LogLevel.Info
+                Warning = log LogLevel.Warning
+                Error = log LogLevel.Error
+            }
+
+        static member inline Default = Logger.Create DEFAULT_LOG_LEVEL
+
+
+    module Logger =
+        let mutable lastLogger = Logger.Default
+        let inline getLogger () = lastLogger
+
+    let inline logTrace fn = Logger.getLogger().Trace fn
+    let inline logDebug fn = Logger.getLogger().Debug fn
+    let inline logInfo fn = Logger.getLogger().Info fn
+    let inline logWarning fn = Logger.getLogger().Warning fn
+    let inline logError fn = Logger.getLogger().Error fn
+
+    logInfo (fun () -> $"Logger. deviceInfo={JS.JSON.stringify Dom.deviceInfo}")
